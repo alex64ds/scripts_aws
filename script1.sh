edf4e7de6f8be7ff3f96ab1fe12ea28745add12d
alex64ds <alex64ds@gmail.com>
2025-10-29 19:19:23 +0100

    SOLO VPC funcional

--- script1.sh
+++ script1.sh
@@ -1,7 +1,15 @@
 # CREO VPC Y DEVUELVO SU ID
 
-VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.0.0/24 \
-    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]' \
+VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.1.0/24 \
+    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=NUBEALEX}]' \
      --query Vpc.VpcId --output text) 
 
-   
+# muestro ID de la VPC
+
+echo "se ha lanzado una nueva VPC | ID -> $VPC_ID"
+
+# HABILITO DNS en LA VPC
+
+aws ec2 modify-vpc-attribute \
+    --vpc-id $VPC_ID \
+    --enable-dns-hostnames "{\"Value\":true}"
