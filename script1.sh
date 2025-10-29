ea338438ed1b39a4090e2e712b851a4342b015a4
alex64ds <alex64ds@gmail.com>
2025-10-29 18:26:00 +0100

    Subo la primera parte del script

--- script1.sh
+++ script1.sh
@@ -1,4 +1,4 @@
-# aws ec2 create-vpc --cidr-block 192.168.0.0/24 \
-#     --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]' \
-#      --query Vpc.VpcId --output text 
+aws ec2 create-vpc --cidr-block 192.168.0.0/24 \
+    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]' \
+     --query Vpc.VpcId --output text 
    
