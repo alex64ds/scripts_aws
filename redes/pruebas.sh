67fd897afe139f51b65d282e79456188dc2ac8c3
alex64ds <alex64ds@gmail.com>
2025-11-05 18:50:17 +0100

    Añado GS en el EC2

--- redes/pruebas.sh
+++ redes/pruebas.sh
@@ -30,42 +30,11 @@ aws ec2 modify-subnet-attribute --subnet-id $SUB_ID --map-public-ip-on-launch
 
 # Creo grupo de seguridad 
 
-SG_ID=$(aws ec2 create-security-group \
+SG_ID=$(aws ec2 create-security-group $VPC_ID \
     --group-name gsalex \
     --description "Mi grupo de seguridad para abrir el puerto 22" \
     --vpc-id $VPC_ID \
-    --query GroupId \
-    --output text)
+    --query GroupId --output text)
 
 
 echo "se ha creado un nuevo grupo de seguridad | ID -> $SG_ID"
-
-# Autorizo el SSH a la instancia
-
-aws ec2 authorize-security-group-ingress \
-    --group-id $SG_ID \
-    --ip-permissions '[{"IpProtocol": "tcp","FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "0.0.0.0/0", "Description": "AllowIp"}]}]' > /dev/null
-
-echo "Se ha habilitado el puerto 22 a $SG_ID"
-
-# Creo EC2
-
-EC2_ID=$(aws ec2 run-instances \
-    --image-id ami-0360c520857e3138f \
-    --instance-type t3.micro \
-    --key-name vockey \
-    --subnet-id $SUB_ID \
-    --security-group-ids $SG_ID \
-    --associate-public-ip-address \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MiEC2}]' \
-    --query Instances.InstanceId --output text)
-
-
-sleep 15
-
-
-echo "se ha lanzado una nueva instancia EC2 | ID -> $EC2_ID"
-
-
-
-
