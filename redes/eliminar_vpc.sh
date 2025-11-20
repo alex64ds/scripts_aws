c671d3571a96edb5e6b56d2605b6a89d59acfea8
Alex64ds <you@example.com>
2025-11-20 13:18:06 +0100

    Eliminador añadido EC2

--- redes/eliminar_vpc.sh
+++ redes/eliminar_vpc.sh
@@ -11,30 +11,47 @@ for VPC_ID in $VPC_IDS; do
     # Eliminar recursos asociados (puentes de internet, subredes, etc.) antes de eliminar la VPC
     # Ejemplo: elimina subredes
     SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" --query "Subnets[*].SubnetId" --output text)
+    echo $SUBNET_IDS
     for SUBNET_ID in $SUBNET_IDS; do
         EC2_SN=$(aws ec2 describe-instances \
             --query Reservations[].Instances[].NetworkInterfaces[].SubnetId \
-            --output text | grep subnet-0be259b7524fba0fa | wc -l)
+            --output text | grep $SUBNET_ID | wc -l)
+        echo $SUBNET_ID
         if [ $EC2_SN -ne 0 ]; then
 
+
             aws ec2 describe-instances \
-                --filters "Name=subnet-id,Values=subnet-0be259b7524fba0fa" \
+                --filters "Name=subnet-id,Values=$SUBNET_ID" \
                 --query Reservations[].Instances[].InstanceId \
                 --output text | tr '\t' '\n' > instancias
-            # while read line; do
+            while read line; do
 
+            comterec2=$(aws ec2 describe-instances \
+                --instance-ids $line  \
+                --query Reservations[].Instances[].State.Name \
+                --output text)
+            
+            if [ $comterec2 != "terminated" ]; then
 
+                aws ec2 terminate-instances \
+                    --instance-ids $line > /dev/null
 
-            # done < instancias
+                echo "Terminando instancia $line"
+                aws ec2 wait instance-terminated \
+                    --instance-ids $line
+                echo "Instancia $line terminada"
+            fi
+
+            done < instancias
+            rm instancias
         fi
-        # aws ec2 delete-subnet --subnet-id $SUBNET_ID
-        # echo " Subnet $SUBNET_ID eliminada."
+        aws ec2 delete-subnet --subnet-id $SUBNET_ID
+        echo " Subnet $SUBNET_ID eliminada."
     done
     
-    # (Opcional) Elimina más recursos aquí como Internet Gateways, Route Tables, etc., si existen
     
     # Elimina la VPC
-    # aws ec2 delete-vpc --vpc-id $VPC_ID
-    # echo "VPC $VPC_ID eliminada."
+    aws ec2 delete-vpc --vpc-id $VPC_ID
+    echo "VPC $VPC_ID eliminada."
 done
-rm instancias
+
