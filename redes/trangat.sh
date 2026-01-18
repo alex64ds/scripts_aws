3b80c91aecb42d435083afd54fa351c38a1186ca
Alex64ds <you@example.com>
2026-01-18 13:19:27 +0100

    Añadimos los transit gateways de forma correcta

--- redes/trangat.sh
+++ redes/trangat.sh
@@ -272,3 +272,227 @@ echo "Se ha habilitado el puerto 22 a $SGore_ID2"
 aws ec2 authorize-security-group-ingress \
     --group-id $SGore_ID2 --region us-west-2 \
     --ip-permissions '[{"IpProtocol": "icmp","FromPort": -1,"ToPort": -1,"IpRanges": [{"CidrIp": "0.0.0.0/0","Description": "Allow_All_ICMP"}]}]' > /dev/null
+
+# Creo EC2
+
+EC2vir_ID1=$(aws ec2 run-instances \
+    --image-id ami-0360c520857e3138f \
+    --instance-type t3.micro \
+    --key-name vockey \
+    --subnet-id $SUB1vir_ID \
+    --security-group-ids $SGvir_ID1 \
+    --associate-public-ip-address \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MiEC2publicovir}]' \
+    --query Instances[].InstanceId --output text)
+
+
+echo "se ha lanzado una nueva instancia EC2 | ID -> $EC2vir_ID1"
+
+EC2ore_ID1=$(aws ec2 run-instances \
+    --image-id ami-00f46ccd1cbfb363e \
+    --instance-type t3.micro \
+    --region us-west-2 \
+    --subnet-id $SUB1ore_ID \
+    --security-group-ids $SGore_ID1 \
+    --associate-public-ip-address \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MiEC2publicoore}]' \
+    --query Instances[].InstanceId --output text)
+
+
+echo "se ha lanzado una nueva instancia EC2 | ID -> $EC2ore_ID1"
+
+EC2vir_ID2=$(aws ec2 run-instances \
+    --image-id ami-0360c520857e3138f \
+    --instance-type t3.micro \
+    --key-name vockey \
+    --subnet-id $SUB2vir_ID \
+    --security-group-ids $SGvir_ID2 \
+    --associate-public-ip-address \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MiEC2publicovir}]' \
+    --query Instances[].InstanceId --output text)
+
+
+echo "se ha lanzado una nueva instancia EC2 | ID -> $EC2vir_ID2"
+
+EC2ore_ID2=$(aws ec2 run-instances \
+    --image-id ami-00f46ccd1cbfb363e \
+    --instance-type t3.micro \
+    --region us-west-2 \
+    --subnet-id $SUB2ore_ID \
+    --security-group-ids $SGore_ID2 \
+    --associate-public-ip-address \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MiEC2publicoore}]' \
+    --query Instances[].InstanceId --output text)
+
+
+echo "se ha lanzado una nueva instancia EC2 | ID -> $EC2ore_ID2"
+
+# Creamos los transits gateways
+
+TGvir=$(aws ec2 create-transit-gateway \
+    --description MyTGW \
+    --options AmazonSideAsn=64516,AutoAcceptSharedAttachments=enable,DefaultRouteTableAssociation=enable,DefaultRouteTablePropagation=enable,VpnEcmpSupport=enable,DnsSupport=enable \
+    --query 'TransitGateway.TransitGatewayId' \
+    --output text)
+
+
+echo "Se ha creado un transit gateway | ID -> $TGvir"
+
+TGvir_state=$(aws ec2 describe-transit-gateways --transit-gateway-ids $TGvir --query "TransitGateways[*].State" --output text )
+
+while [ $TGvir_state != "available" ]; do
+
+    TGvir_state=$(aws ec2 describe-transit-gateways --transit-gateway-ids $TGvir --query "TransitGateways[*].State" --output text )
+
+done
+
+echo "$TGvir ya disponible"
+
+TGore=$(aws ec2 create-transit-gateway \
+    --description MyTGW \
+    --options AmazonSideAsn=64516,AutoAcceptSharedAttachments=enable,DefaultRouteTableAssociation=enable,DefaultRouteTablePropagation=enable,VpnEcmpSupport=enable,DnsSupport=enable \
+    --region us-west-2 \
+    --query 'TransitGateway.TransitGatewayId' \
+    --output text)
+
+echo "Se ha creado un transit gateway | ID -> $TGore"
+
+TGore_state=$(aws ec2 describe-transit-gateways --transit-gateway-ids $TGore --query "TransitGateways[*].State" --region us-west-2 --output text )
+
+while [ $TGore_state != "available" ]; do
+
+    TGore_state=$(aws ec2 describe-transit-gateways --transit-gateway-ids $TGore --query "TransitGateways[*].State" --region us-west-2 --output text )
+
+done
+
+echo "$TGore ya disponible"
+
+# Creamos los attachments para los transit gateways
+
+
+ATT1_VIR=$(aws ec2 create-transit-gateway-vpc-attachment \
+    --transit-gateway-id $TGvir \
+    --vpc-id $VPC1_IDvir \
+    --subnet-id $SUB1vir_ID \
+    --query 'TransitGatewayVpcAttachment.TransitGatewayAttachmentId' \
+    --output text)
+
+echo "Attachment creado: $ATT1_VIR "
+
+
+# Esperar ATT1_VIR
+STATE=$(aws ec2 describe-transit-gateway-vpc-attachments \
+    --transit-gateway-attachment-ids $ATT1_VIR \
+    --query 'TransitGatewayVpcAttachments[0].State' \
+    --output text)
+
+while [ "$STATE" != "available" ]; do
+    STATE=$(aws ec2 describe-transit-gateway-vpc-attachments \
+        --transit-gateway-attachment-ids $ATT1_VIR \
+        --query 'TransitGatewayVpcAttachments[0].State' \
+        --output text)
+done
+
+echo " $ATT1_VIR ya esta disponible."
+
+
+ATT2_VIR=$(aws ec2 create-transit-gateway-vpc-attachment \
+    --transit-gateway-id $TGvir \
+    --vpc-id $VPC2_IDvir \
+    --subnet-id $SUB2vir_ID \
+    --query 'TransitGatewayVpcAttachment.TransitGatewayAttachmentId' \
+    --output text)
+
+echo "Attachment creado: $ATT2_VIR "
+
+
+# Esperar ATT2_VIR
+STATE=$(aws ec2 describe-transit-gateway-vpc-attachments \
+    --transit-gateway-attachment-ids $ATT2_VIR \
+    --region us-east-1 \
+    --query 'TransitGatewayVpcAttachments[0].State' \
+    --output text)
+
+while [ "$STATE" != "available" ]; do
+    STATE=$(aws ec2 describe-transit-gateway-vpc-attachments \
+        --transit-gateway-attachment-ids $ATT2_VIR \
+        --query 'TransitGatewayVpcAttachments[0].State' \
+        --output text)
+done
+
+echo "$ATT2_VIR ya esta disponible."
+
+
+ATT1_ORE=$(aws ec2 create-transit-gateway-vpc-attachment \
+    --transit-gateway-id $TGore \
+    --vpc-id $VPC1_IDore \
+    --subnet-id $SUB1ore_ID \
+    --region us-west-2 \
+    --query 'TransitGatewayVpcAttachment.TransitGatewayAttachmentId' \
+    --output text)
+
+echo "Attachment creado: $ATT1_ORE"
+
+
+# Esperar ATT1_ORE
+STATE=$(aws ec2 describe-transit-gateway-vpc-attachments \
+    --transit-gateway-attachment-ids $ATT1_ORE \
+    --region us-west-2 \
+    --query 'TransitGatewayVpcAttachments[0].State' \
+    --output text)
+
+while [ "$STATE" != "available" ]; do
+    STATE=$(aws ec2 describe-transit-gateway-vpc-attachments \
+        --transit-gateway-attachment-ids $ATT1_ORE \
+        --region us-west-2 \
+        --query 'TransitGatewayVpcAttachments[0].State' \
+        --output text)
+done
+
+echo "$ATT1_ORE ya disponible."
+
+ATT2_ORE=$(aws ec2 create-transit-gateway-vpc-attachment \
+    --transit-gateway-id $TGore \
+    --vpc-id $VPC2_IDore \
+    --subnet-id $SUB2ore_ID \
+    --region us-west-2 \
+    --query 'TransitGatewayVpcAttachment.TransitGatewayAttachmentId' \
+    --output text)
+
+echo "Attachment creado: $ATT2_ORE"
+
+
+# Esperar ATT2_ORE
+STATE=$(aws ec2 describe-transit-gateway-vpc-attachments \
+    --transit-gateway-attachment-ids $ATT2_ORE \
+    --region us-west-2 \
+    --query 'TransitGatewayVpcAttachments[0].State' \
+    --output text)
+
+while [ "$STATE" != "available" ]; do
+    STATE=$(aws ec2 describe-transit-gateway-vpc-attachments \
+        --transit-gateway-attachment-ids $ATT2_ORE \
+        --region us-west-2 \
+        --query 'TransitGatewayVpcAttachments[0].State' \
+        --output text)
+done
+
+echo "$ATT2_ORE ya disponible."
+
+
+
+aws ec2 create-route --route-table-id $RTBPUBvir_ID1 \
+    --destination-cidr-block 10.2.0.0/16 \
+    --transit-gateway-id $TGvir > /dev/null
+
+aws ec2 create-route --route-table-id $RTBPUBvir_ID2 \
+    --destination-cidr-block 10.1.0.0/16  \
+    --transit-gateway-id $TGvir > /dev/null
+
+aws ec2 create-route --route-table-id $RTBPUBore_ID1 \
+    --destination-cidr-block 192.224.0.0/16 \
+    --transit-gateway-id $TGore --region us-west-2 > /dev/null
+
+aws ec2 create-route --route-table-id $RTBPUBore_ID2 \
+    --destination-cidr-block 192.168.0.0/16 \
+    --transit-gateway-id $TGore --region us-west-2 > /dev/null
